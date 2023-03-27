9816f6853f4b6f009cfb4b2b3f47925af5acd12f
Shreya Ranjan (CP1404) <shreya.ranjan@my.jcu.edu.au>
2023-03-27 11:30:44 +0800

    Add password check program

---
+++ password_stars.py
@@ -0,0 +1,18 @@
+def main():
+    """Check password."""
+    MIN_LENGTH = 6
+    password = get_password()
+    while len(password) < MIN_LENGTH:
+        password = input("Enter password: ")
+    display_asterisks(password)
+
+def get_password():
+    """Input password."""
+    password = input("Enter password: ")
+    return password
+
+def display_asterisks(password):
+    """Display asterisks."""
+    print("*" * len(password))
+
+main()
